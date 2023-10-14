9ff7748036c3fe57092b4b2ad1c85326bcdca545
D4lister <33123196+72haja@users.noreply.github.com>
2023-10-14 13:47:47 +0000

    extract component as responsiveFieldWrapper

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,13 @@
 import { component$ } from "@builder.io/qwik";
 
-import Field from "./components/field/field.tsx";
+import ResponsiveFieldWrapper from "./components/responsiveFieldWrapper/responsiveFieldWrapper.tsx";
 import "./app.css";
 
 export const App = component$(() => {
   return (
     <>
       <div class="flex items-center justify-center w-full h-full">
-        <Field></Field>
+        <ResponsiveFieldWrapper />
       </div>
     </>
   );
