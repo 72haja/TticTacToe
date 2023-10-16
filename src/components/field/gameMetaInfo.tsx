c25b247bbfe656cd0d0eb4fa3db1996b87de37d7
Jakob Haag <Jakob.haag@spicetech.de>
2023-10-16 12:18:45 +0200

    css

--- src/components/field/gameMetaInfo.tsx
+++ src/components/field/gameMetaInfo.tsx
@@ -2,6 +2,8 @@ import { component$ } from "@builder.io/qwik";
 import Player1Icon from "./Player1Icon";
 import Player2Icon from "./Player2Icon";
 
+import "./gameMetaInfo.css";
+
 interface ItemProps {
   playerIcon: string;
   activePlayer: string;
@@ -10,15 +12,15 @@ interface ItemProps {
 
 export default component$<ItemProps>((props) => {
   return (
-    <div class="grid grid-cols-2 w-full">
-      <div class="flex items-center gap-2 w-full h-[50px]">
-        <span>Your Icon: </span>
+    <div class="grid grid-cols-2 w-full" id="meta-game-info-wrapper">
+      <div class="flex items-center gap-2 w-full lg:h-[50px] md:h-[50px] sm:h-[35px] h-[25px]">
+        <span class="whitespace-nowrap">Your Icon: </span>
         <Player1Icon
           playerIcon={props.playerIcon}
           size="h-full"
         />
       </div>
-      <div class="flex items-center gap-2 w-full h-[50px]">
+      <div class="flex items-center gap-2 w-full lg:h-[50px] md:h-[50px] sm:h-[35px] h-[25px]">
         <span>activePlayer: </span>
         {props.activePlayer === props.player
           ? <Player1Icon
--- src/components/responsiveFieldWrapper/responsiveFieldWrapper.tsx
+++ src/components/responsiveFieldWrapper/responsiveFieldWrapper.tsx
@@ -4,7 +4,7 @@ import "./responsiveFieldWrapper.css";
 
 export default component$(() => {
   return (
-    <div class="w-full h-full flex flex-col gap-5 items-center justify-center ">
+    <div id="square-wrapper" class="w-full h-full flex flex-col gap-5 items-center justify-center ">
       <div id="square">
         <Slot />
       </div>
